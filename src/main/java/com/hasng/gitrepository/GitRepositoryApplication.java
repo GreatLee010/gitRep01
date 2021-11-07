62b0fc35290513c8541365dea5fdb46e275e003b
lihuibo <865009659@qq.com>
2021-11-07 21:24:38 +0800

    第一次提交

---
+++ src/main/java/com/hasng/gitrepository/GitRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.hasng.gitrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasng/gitrepository/GitRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasng.gitrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
